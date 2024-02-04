558a3442b9b954884eaf616d10367088768a12ca
antoniocristovam <antonioristovam@outlook.com.br>
2024-02-04 13:15:19 -0300

    feat: adjustment clientsModel

--- src/domain/models/clients-model.ts
+++ src/domain/models/clients-model.ts
@@ -14,3 +14,27 @@ export type ClientsModel = {
     },
   ];
 };
+
+export type ClientsByIdModel = {
+  cpf_cnpj: string;
+  name: string;
+  email: string;
+  firstPhoneNumber: string;
+  secondPhoneNumber: string;
+  thirdPhoneNumber: string;
+  addresses: [
+    {
+      id: string;
+      postalCode: string;
+      state: string;
+      street: string;
+      city: string;
+      neighborhood: string;
+      number: string;
+      aditionalInformation: string;
+      parish: string;
+      community: string;
+      clientId: string;
+    },
+  ];
+};
