666335b644ac36afaf3b3a4abec213fd4d9805ec
Antonio Cristovam <antoniocristovam@Antonios-iMac-Pro.local>
2024-02-10 12:16:38 -0800

    feat: adjustment modal delete

--- src/presentation/app/components/modal-delete/index.tsx
+++ src/presentation/app/components/modal-delete/index.tsx
@@ -40,23 +40,26 @@ export const ModalDelete = ({ goBack, handleDelete, isOpen }: IProps) => {
             <p className="text-muted mb-4">Tem certeza que deseja excluir?</p>
           </Row>
 
-          <Row className="d-flex justify-content-center" lg={6}>
-            <Col xs={3} md={3} lg={3}>
+          <Row
+            className="d-flex justify-content-center align-content-center"
+            lg={12}
+          >
+            <Col xs={2} md={2} lg={2}>
               <button
                 type="button"
-                className="btn btn-link"
+                className="btn btn-success"
                 onClick={() => goBack()}
               >
-                Cancelar
+                Não
               </button>
             </Col>
-            <Col xs={3} md={3} lg={3}>
+            <Col xs={2} md={2} lg={2}>
               <Button
                 type="button"
                 className="btn btn-danger"
                 onClick={() => handleDelete()}
               >
-                Excluir
+                Sim
               </Button>
             </Col>
           </Row>
